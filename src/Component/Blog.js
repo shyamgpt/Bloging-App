fa6a040bd893ae08afc8e067642bd093346d7685
unknown <shyamgupta1.nmims@gmail.com>
2024-01-05 02:47:53 +0530

    passing an object in useState(), In this we will setState in one line instead of two line

--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -2,8 +2,12 @@ import {useState} from "react";
 //Blogging App using Hooks
 export default function Blog(){
 
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+    // const [title, setTitle] = useState("");
+    // In this we assign intial value as an object
+    // const [content, setContent] = useState("");
+
+    const [formData, setFormData] = useState({title:"",content:""})
+    //In this we will assign value as object not string
     const [blogs, setBlogs] = useState([]);
     
     //Passing the synthetic event as argument to stop refreshing the page on submit
@@ -12,8 +16,14 @@ export default function Blog(){
 
         // setBlogs({title, content});
         // Now using rest oprerator -->
-        setBlogs([{title,content},...blogs]);
-        console.log(blogs);
+        // setBlogs([{title,content},...blogs]);
+        // setTitle("");
+        // setContent("");
+        // console.log(blogs);
+
+        setBlogs([{title:formData.title, content:formData.content},...blogs]);
+        setFormData({title:"", content:""})
+       
         //Link to understand this topic -->https://www.youtube.com/watch?v=DoIGxx7P-ps
     }
 
@@ -32,8 +42,10 @@ export default function Blog(){
                 <Row label="Title">
                         <input className="input"
                                 placeholder="Enter the Title of the Blog here.."
-                                value ={title}
-                                onChange ={(e)=> setTitle(e.target.value)}
+                                // value ={title}
+                                // onChange ={(e)=> setTitle(e.target.value)}
+                                value ={formData.title}
+                                onChange ={(e)=> setFormData({title: e.target.value, content: formData.content})}
                                 />
                 </Row >
 
@@ -41,8 +53,11 @@ export default function Blog(){
                 <Row label="Content">
                         <textarea className="input content"
                                 placeholder="Content of the Blog goes here.."
-                                value ={content}
-                                onChange ={(e) => setContent(e.target.value)}
+                                // value ={content}
+                                // onChange ={(e) => setContent(e.target.value)}
+
+                                value ={formData.content}
+                                onChange ={(e) => setFormData({title: formData.title, content: e.target.value})}
                                 />
                 </Row >
 
