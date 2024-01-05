38ffc67c205626f8a2cd410db1c4981b23bac042
unknown <shyamgupta1.nmims@gmail.com>
2024-01-06 00:54:31 +0530

    Setting of focus on input filed using useRef

--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useRef} from "react";
 //Blogging App using Hooks
 export default function Blog(){
 
@@ -10,6 +10,7 @@ export default function Blog(){
     const [formData, setFormData] = useState({title:"",content:""})
     //In this we will assign value as object not string
     const [blogs, setBlogs] = useState([]);
+    const titleRef = useRef(null);
     
     //Passing the synthetic event as argument to stop refreshing the page on submit
     function handleSubmit(e){
@@ -21,6 +22,7 @@ export default function Blog(){
          */
         setBlogs([{title:formData.title, content:formData.content},...blogs]);
         setFormData({title:"", content:""})
+        titleRef.current.focus();
       
     }
 
@@ -48,7 +50,8 @@ export default function Blog(){
                                 // onChange ={(e)=> setTitle(e.target.value)}
                                 value ={formData.title}
                                 onChange ={(e)=> setFormData({title: e.target.value, content: formData.content})}
-                                />
+                               ref = {titleRef}
+                               />
                 </Row >
 
                 {/* Row component to create a row for Text area field */}
